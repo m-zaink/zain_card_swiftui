4d301c02926db6af2c14aee6e3a4d3df5a1b744d
m-zaink <sidsmith9@gmail.com>
2020-11-21 11:11:30 +0530

    Initial Commit

---
+++ ZainCard/ZainCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZainCardApp.swift
+//  ZainCard
+//
+//  Created by Mohammed Sadiq on 21/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ZainCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
