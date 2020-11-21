47ab4e55c31f6061dd528be450df8d0448217f70
m-zaink <sidsmith9@gmail.com>
2020-11-21 11:26:05 +0530

    Set up project | FC

--- ZainCard/ContentView.swift
+++ ZainCard/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("ZainKhanProfilePicture")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300, alignment: .center)
+                    .clipShape(Circle())
+                Text("Zain Khan")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("Flutter & iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+            }
+            
+        }
     }
 }
 
